62d8a0a8a29ca721050aa8de7f5ba2a52add9b7c
Luis Trivinho <95496868+luisTrivinh0@users.noreply.github.com>
2026-09-02 22:28:07 -0300

    refactor: centraliza configuração do site

---
+++ site.config.js
@@ -0,0 +1,20 @@
+window.SITE_CONFIG = {
+  repository: "luisTrivinh0/reginaldo-trivinho",
+  branch: "main",
+  brand: {
+    name: "Reginaldo Trivinho",
+    initials: "RT",
+    role: "Assistente Técnico Jurídico"
+  },
+  contact: {
+    phoneE164: "5511972670073",
+    phoneDisplay: "+55 11 97267-0073",
+    emailPrimary: "reginaldo.trivinho@icloud.com",
+    emailSecondary: "retrivinho@gmail.com",
+    whatsappMessage: "Olá, Reginaldo. Gostaria de conversar sobre assistência técnica jurídica."
+  },
+  poweredBy: {
+    label: "Powered by Retorna",
+    url: "https://retornaservicos.com.br"
+  }
+};
