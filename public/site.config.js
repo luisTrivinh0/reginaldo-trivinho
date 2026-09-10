7e5e53a41ad5ffa865baf2aaaf856e7ffd3d2a44
Luis Trivinho <95496868+luisTrivinh0@users.noreply.github.com>
2026-09-10 19:41:54 -0300

    feat: reposiciona identidade profissional e inclui CREA

--- public/site.config.js
+++ public/site.config.js
@@ -2,14 +2,15 @@ window.SITE_CONFIG = {
   brand: {
     name: "Reginaldo Trivinho",
     initials: "RT",
-    role: "Assistente Técnico Jurídico"
+    role: "Engenheiro Eletrônico | Especialista em Perícias Judiciais",
+    crea: "CREA-SP nº 5069843080"
   },
   contact: {
     phoneE164: "5511972670073",
     phoneDisplay: "+55 11 97267-0073",
     emailPrimary: "reginaldo.trivinho@icloud.com",
     emailSecondary: "retrivinho@gmail.com",
-    whatsappMessage: "Olá, Reginaldo. Gostaria de conversar sobre assistência técnica jurídica."
+    whatsappMessage: "Olá, Reginaldo. Gostaria de apresentar uma demanda relacionada a perícia ou assistência técnica em Engenharia Eletrônica."
   },
   poweredBy: {
     label: "Powered by Retorna",
