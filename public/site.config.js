fd1df36df2346656d0eb88b0aa0566d07c12d9f7
Luis Trivinho <95496868+luisTrivinh0@users.noreply.github.com>
2026-09-23 20:46:32 -0300

    feat: amplia perfil profissional com dados do curriculo

--- public/site.config.js
+++ public/site.config.js
@@ -2,12 +2,14 @@ window.SITE_CONFIG = {
   brand: {
     name: "Reginaldo Trivinho",
     initials: "RT",
-    role: "Engenheiro Eletrônico | Especialista em Perícias Judiciais",
+    role: "Engenheiro Eletrônico | Perícias Judiciais | Manutenção Industrial",
     crea: "CREA-SP nº 5069843080"
   },
   contact: {
     phoneE164: "5511972670073",
     phoneDisplay: "+55 11 97267-0073",
+    phoneSecondaryE164: "5511971386332",
+    phoneSecondaryDisplay: "+55 11 97138-6332",
     emailPrimary: "reginaldo.trivinho@icloud.com",
     emailSecondary: "retrivinho@gmail.com",
     whatsappMessage: "Olá, Reginaldo. Gostaria de apresentar uma demanda relacionada a Perícia Judicial ou assistência técnica em Engenharia Eletrônica."
